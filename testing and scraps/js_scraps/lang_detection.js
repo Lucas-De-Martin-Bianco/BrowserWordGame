dcc41b48a59b1f295953a924f179a796c10644e5
bat0slurp <85578682+Lucas-De-Martin-Bianco@users.noreply.github.com>
2024-05-08 11:47:22 +0200

    deleting useless files

--- testing and scraps/js_scraps/lang_detection.js
+++
@@ -1,5 +0,0 @@
-// Get the selected language
-const selectedLanguage = document.querySelector('input[name="btnlang"]:checked').nextElementSibling.textContent;
-
-// Log the selected language
-console.log(selectedLanguage);
